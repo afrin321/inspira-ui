2c1231a65246767968861123da1192c718e6a684
AfrinH <afrinhaque@outlook.com>
2023-12-14 06:17:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='w-full h-full bg-first-screen'>
+
+    </div>
+  )
+}
+
+export default App
