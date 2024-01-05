fd012ce6a176e4246aec6864251acfd86daf8ecc
AfrinH <afrinhaque@outlook.com>
2024-01-05 21:33:31 +0600

    Contact page progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import LinkedIn from './../assets/linkedin_logo.png'
 
 function Footer() {
   return (
-    <footer className='w-full flex justify-between footer absolute bottom-0 px-[8%]'>
+    <footer className='w-full flex justify-around footer absolute bottom-0 py-3'>
         <div className='flex w-52 flex-col py-4 gap-2'>
             <img src={Logo} className='my-2 w-24 ' />
             <div className='flex gap-2'>
@@ -13,8 +13,20 @@ function Footer() {
                 <img src={LinkedIn} className='w-8 h-8' />
             </div>
         </div>
-        <div className='flex w-52 '>
-            <div></div>
+        <div className='flex w-[60%] gap-10 justify-end px-16 text-sm'>
+            <div className='flex flex-col gap-1 text-white py-3 w-[200px]'>
+                <span>POLITICA DE PRIVACIDAD</span>
+                <span>AVISO LEGAL</span>
+                <span>POLITICA DE COOKIES</span>
+            </div>
+            <div className='flex flex-col gap-1 whitespace-nowrap text-white py-3 w-[200px] '>
+                <span>HOME</span>
+                <span>¿QUÉ NOS INSPIRA?</span>
+                <span>¿QUÉ HACEMOS? </span>
+                <span className=''>CASOS QUE NOS SIGUEN INSPIRANDO </span>
+                <span>¿QUIÉN NOS INSPIRA? </span>
+                <span>PLATIQUEMOS</span>
+            </div>
         </div>
     </footer>
   )
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import OtherNav from '../components/OtherNav'
 import Footer from '../components/Footer'
+import Graphics from './../assets/graphics_3.png'
 
 function Contact() {
   return (
     <section className='relative bg-[#E4E3DB] m-0 p-0 flex flex-col w-full h-screen'>
       <OtherNav/>
-      <div className='w-full clear-start overflow-hidden relative'>
-        
+      <div className='w-full clear-start overflow-hidden relative flex gap-5 '>
+        <img src={Graphics} className='w-auto h-full' />
+        <div className='w-full flex flex-col items-end gap-2 p-8 px-12'>
+          <p className='text-5xl text-[#F7073E] flex justify-end'>PLATIQUEMOS</p>
+          <div className='flex flex-col justify-end items-end'>
+            <input className='w-[80vh] border-[#707070] border-[1px] outline-none' />
+          </div>
+        </div>
       </div>
       <Footer/>
     </section>
