9e317d2e7eb9c419eac1d7c96de9cc8fcded709b
AfrinH <afrinhaque@outlook.com>
2024-01-10 10:44:26 +0600

    new design updated

---
+++ src/components/CaseCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import One from './../assets/01_img.png'
+import Two from './../assets/02_img.png'
+import Three from './../assets/03_img.png'
+import Four from './../assets/04_img.png'
+
+function CaseCard(props) {
+  const { data } = props
+  const { img, title, description, isHover } = data
+
+  const imgMap = new Map()
+  imgMap.set('1', One)
+  imgMap.set('2', Two)
+  imgMap.set('3', Three)
+  imgMap.set('4', Four)
+
+  return (
+    <div className='flex flex-col m-0 p-0 w-full h-full rounded-xl bg-transparent'>
+        <div className='h-[65%] w-full flex justify-center'>
+            {
+                !isHover && <span className='veritical text-[13px] font-bold text-white mt-6'>{title}</span>
+            }
+        </div>
+        <div className='m-0 p-0 flex justify-between h-[35%] w-full'>
+            <div className='m-0 p-0 flex pl-10 '>
+                {
+                    isHover &&
+                    <div className='flex flex-col gap-1 items-start'>
+                        <p className="text-white font-bold text-xl">{title}</p>
+                        <p className='text-white text-[12px] font-semibold'>{description}</p>
+                        <button className='my-5 text-[#F7063E] text-sm rounded-full px-10 py-2 bg-white'>Detalle</button>
+                    </div>
+                }
+            </div>
+            <div className='flex w-auto items-end align-bottom h-full'>
+                <img src={imgMap.get(img)} className='m-1'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default CaseCard
--- src/pages/Qualities.jsx
+++ src/pages/Qualities.jsx
@@ -7,6 +7,7 @@ import Q3 from './../assets/q3.png'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { useRef } from 'react'
+import CaseCard from '../components/CaseCard'
 
 function Qualities() {
 
@@ -16,6 +17,39 @@ function Qualities() {
   const divD = useRef(null)
 
   const [hoverElement, setHoverElement] = useState(divA)
+
+  const dataMap = new Map() 
+  dataMap.set(divA, {
+    img: '1',
+    title: 'Gestión de marca',
+    description: 'Los 2 embotelladores más grandes del sistema Coca-Cola en México (FEMSA y ARCA Continental), son los …',
+    isHover: hoverElement == divA
+   })
+
+   dataMap.set(divB, {
+    img: '2',
+    title: 'Adaptación de empaque',
+    description: 'Los 2 embotelladores más grandes del sistema Coca-Cola en México (FEMSA y ARCA Continental), son los …',
+    isHover: hoverElement == divB
+   })
+
+   dataMap.set(divC, {
+    img: '3',
+    title: 'Comunicación gráfica para POP | OOH',
+    description: 'Los 2 embotelladores más grandes del sistema Coca-Cola en México (FEMSA y ARCA Continental), son los …',
+    isHover: hoverElement == divC
+   })
+
+   dataMap.set(divD, {
+    img: '4',
+    title: 'Gestión de marca ',
+    description: 'Los 2 embotelladores más grandes del sistema Coca-Cola en México (FEMSA y ARCA Continental), son los …',
+    isHover: hoverElement == divD
+   })
+
+
+
+  
    
   return (
     <section id='quality' className='qualities m-0 p-0 flex flex-col w-full h-screen'>
@@ -24,51 +58,50 @@ function Qualities() {
         <div className='py-1'><span className='text-white whitespace-nowrap text-4xl'>CASOS QUE NOS SIGUEN <b>INSPIRANDO</b> </span></div>
         <div className='flex justify-between gap-2 h-full py-2'>
             <motion.div 
-            ref={divA}    
-            onHoverStart={() => setHoverElement(divA)}                   
-            className={`${hoverElement == divA ? 'w-[50%]' : 'w-[16%]'} h-full 
-            rounded-xl case1 p-0`}>
-              <div className='rounded-xl w-full h-full overlay-full overflow-hidden'>
-                <div 
-                className={`${hoverElement != divA && 'overlay-pink'}  rounded-xl w-full h-full overflow-hidden`}>
-                
-                </div>
-              </div>
-
-            </motion.div>
-            <motion.div
-            ref={divB}            
-            onHoverStart={() => setHoverElement(divB)}
-            className={`${hoverElement == divB ? 'w-[50%]' : 'w-[16%]'} h-full 
-            rounded-xl case2 p-0`}>
+              ref={divA}    
+              onHoverStart={() => setHoverElement(divA)}                   
+              className={`${hoverElement == divA ? 'w-[50%]' : 'w-[16%]'} h-full 
+              rounded-xl case1 p-0`}>
                 <div className='rounded-xl w-full h-full overlay-full overflow-hidden'>
                   <div 
-                    className={`${hoverElement != divB && 'overlay-green'}  rounded-xl w-full h-full overflow-hidden`}>
-                  
+                    className={`${hoverElement != divA && 'overlay-pink'}  rounded-xl w-full h-full overflow-hidden`}>
+                      <CaseCard data={dataMap.get(divA)}/>
+                  </div>
+                </div>
+            </motion.div>
+            <motion.div
+              ref={divB}            
+              onHoverStart={() => setHoverElement(divB)}
+              className={`${hoverElement == divB ? 'w-[50%]' : 'w-[16%]'} h-full 
+              rounded-xl case2 p-0`}>
+                  <div className='rounded-xl w-full h-full overlay-full overflow-hidden'>
+                    <div 
+                      className={`${hoverElement != divB && 'overlay-green'}  rounded-xl w-full h-full overflow-hidden`}>
+                         <CaseCard data={dataMap.get(divB)}/>
+                    </div>
+                  </div>
+            </motion.div>
+            <motion.div 
+              onHoverStart={() => setHoverElement(divC)}
+              className={`${hoverElement == divC ? 'w-[50%]' : 'w-[16%]'} h-full 
+              rounded-xl case3 p-0`}>
+                <div className='rounded-xl w-full h-full overlay-full overflow-hidden'>
+                  <div 
+                    className={`${hoverElement != divC && 'overlay-gray'}  rounded-xl w-full h-full overflow-hidden`}>
+                       <CaseCard data={dataMap.get(divC)}/>
                   </div>
                 </div>
             </motion.div>
             <motion.div 
-            onHoverStart={() => setHoverElement(divC)}
-            className={`${hoverElement == divC ? 'w-[50%]' : 'w-[16%]'} h-full 
-            rounded-xl case3 p-0`}>
-              <div className='rounded-xl w-full h-full overlay-full overflow-hidden'>
-                <div 
-                  className={`${hoverElement != divC && 'overlay-gray'}  rounded-xl w-full h-full overflow-hidden`}>
-                
+              onHoverStart={() => setHoverElement(divD)}
+              className={`${hoverElement == divD ? 'w-[50%]' : 'w-[16%]'} h-full 
+              rounded-xl case5 p-0`}>
+                <div className='rounded-xl w-full h-full overlay-full overflow-hidden'>
+                  <div 
+                    className={`${hoverElement != divD && 'overlay-violet'}  rounded-xl w-full h-full overflow-hidden`}>
+                      <CaseCard data={dataMap.get(divD)}/>
+                  </div>
                 </div>
-              </div>
-            </motion.div>
-            <motion.div 
-            onHoverStart={() => setHoverElement(divD)}
-            className={`${hoverElement == divD ? 'w-[50%]' : 'w-[16%]'} h-full 
-            rounded-xl case5 p-0`}>
-              <div className='rounded-xl w-full h-full overlay-full overflow-hidden'>
-                <div 
-                  className={`${hoverElement != divD && 'overlay-violet'}  rounded-xl w-full h-full overflow-hidden`}>
-                
-                </div>
-              </div>
             </motion.div>
         </div>
       </div>
